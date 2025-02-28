11b7749a3f7ddaa8bfb3e5be1e48a064b324bcf9
Tech Craft <ga.techcraft@gmail.com>
2025-02-28 21:57:53 +0900

    classifying client

--- client.js
+++ client.js
@@ -1,57 +1,81 @@
 const net = require("net");
-const SERVER_PATH = "server_socket";
 
-const client = new net.Socket();
+class Client {
+  constructor (server_path) {
+    this.server_path = server_path;
+    this.sock = new net.Socket();
+  }
 
-// data = {
-//   "name": "floor",
-//   "params": [1.5],
-// }
+  connect () {
+    // サーバーと接続
+    this.sock.connect(this.server_path, () => {
+      console.log("サーバーに接続しました。");
+      
+      // サーバーからレスポンスを受信
+      this.sock.on("data", (data) => {
+        console.log("レスポンス:", data.toString());
+        
+      });
+      
+      // エラー処理
+      this.sock.on("error", (err) => {
+        console.error("エラー:", err.message);
+      });
+      
+      // 接続終了時の処理
+      this.sock.on("close", () => {
+        console.log("接続を閉じました。");
+      });
+    });
+  }
 
-// data = {
-//   "name": "nroot",
-//   "params": [2, 4],
-// }
+  send_request (data) {
+    const request = JSON.stringify(data);
+    this.sock.write(request);
+    console.log("リクエスト:", data);
+  }
 
-// data = {
-//   "name": "reverse",
-//   "params": ["hello"],
-// }
+  disconnect () {
+    this.sock.end();
+  }
+};
 
-// data = {
-//   "name": "validAnagram",
-//   "params": ["hello", "olleh"],
-// }
-
-data = {
-  "name": "sort",
-  "params": ["orange","apple"],
+class DataGenerator {
+  constructor (method) {
+   if (method === "floor") {
+    return {
+      "name": "floor",
+      "params": [1,5]
+    }
+   } else if (method === "nroot") {
+    return {
+      "name": "nroot",
+      "params": [2, 4]
+    }
+   } else if (method === "reverse") {
+    return {
+      "name": "reverse",
+      "params": ["hello"]
+    }
+   } else if (method === "validAnagram") {
+    return {
+      "name": "validAnagram",
+      "params": ["hello", "olleh"],
+    }
+   } else if (method === "sort") {
+    return {
+      "name": "sort",
+      "params": ["orange","apple"],
+    }
+   }
+  }
 }
 
-// サーバーと接続
-client.connect(SERVER_PATH, () => {
-  console.log("サーバーに接続しました。");
+const SERVER_PATH = "server_socket";
 
-  // サーバーにリクエストを送信
-  const request = JSON.stringify(data);
-  client.write(request);
-  console.log("リクエスト:", data);
-});
+const client = new Client(SERVER_PATH);
+const data = new DataGenerator("floor")
 
-// サーバーからレスポンスを受信
-client.on("data", (data) => {
-  console.log("レスポンス:", data.toString());
-
-  // レスポンス受信後、ソケットを閉じる
-  client.end();
-});
-
-// エラー処理
-client.on("error", (err) => {
-  console.error("エラー:", err.message);
-});
-
-// 接続終了時の処理
-client.on("close", () => {
-  console.log("接続を閉じました。");
-});
+client.connect();
+client.send_request(data);
+client.disconnect();
